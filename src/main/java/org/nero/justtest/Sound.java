d3a882be45e23a1dda7aedbcf2476344b8529878
Shi3ofreniR <t40411304@yandex.ru>
2024-04-28 19:56:48 +0700

    some fixes, removed unused resources, sound events and imports, version updated

--- src/main/java/org/nero/justtest/Sound.java
+++ src/main/java/org/nero/justtest/Sound.java
@@ -6,16 +6,13 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.lwjgl.system.CallbackI;
 
 public class Sound {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, TestModMc.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> POOF = registerSoundEvent("poof");
     public static final RegistryObject<SoundEvent> RECIPE_NOTIFY = registerSoundEvent("recipe_notify");
     public static final RegistryObject<SoundEvent> ADV_NOTIFY = registerSoundEvent("adv_notify");
     public static final RegistryObject<SoundEvent> GOAL_NOTIFY = registerSoundEvent("goal_notify");
-    public static final RegistryObject<SoundEvent> TUTOR_NOTIFY = registerSoundEvent("tutor_notify");
 
     private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
         return SOUND_EVENTS.register(name, () -> new SoundEvent(new ResourceLocation(TestModMc.MOD_ID, name)));
--- src/main/java/org/nero/justtest/TestModMc.java
+++ src/main/java/org/nero/justtest/TestModMc.java
@@ -1,49 +1,15 @@
 package org.nero.justtest;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraft.sounds.SoundSource;
-
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.HashSet;
-import java.util.Set;
 
 @Mod(TestModMc.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TestModMc
 {
-
-
-
-//    private final Set<Entity> deadEntities = new HashSet<>();
-//    @SubscribeEvent
-//    public void livingDeath(LivingDeathEvent event) {
-//        if (deadEntities.add(event.getEntityLiving())) {
-//            // Получение координат моба
-//            double x = event.getEntityLiving().getX();
-//            double y = event.getEntityLiving().getY();
-//            double z = event.getEntityLiving().getZ();
-//            Level world = event.getEntityLiving().level;
-//
-//            Timer timer = new Timer();
-//            timer.schedule(new TimerTask() {
-//                @Override
-//                public void run() {
-//                    world.playSound(null, x, y, z, Sound.POOF.get(), SoundSource.HOSTILE, 1.0f, 1.0f);
-//                }
-//            }, 1000);
-//        }
-//
-//    }
-//
 
 
 
